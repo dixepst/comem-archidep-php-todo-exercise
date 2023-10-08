073d64a4c6c097f44bc005c37b22256100da2d61
Abel Gomez <abelgomez@AbelGomez.local>
2023-10-08 15:05:12 +0200

    Rajout de l'ordre des tâches (plus récent au plus anciens), modification des connexions à la bdd

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 define('BASE_URL','/ARCHIDEP/comem-archidep-php-todo-exercise/');
 
 // Database connection parameters.
-define('DB_USER', 'todolist');
-define('DB_PASS', 'chAngeMeN0w!');
-define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'archidep');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '8889');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
@@ -77,7 +77,7 @@ if (isset($_POST['action'])) {
 /**
  * Select all tasks from the database.
  */
-$selectQuery = 'SELECT * FROM todo'; // Selectionner toutes les taches
+$selectQuery = 'SELECT * FROM todo ORDER BY created_at DESC'; // Selectionner toutes les taches
 $items = $db->query($selectQuery);
 ?>
 
